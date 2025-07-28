3e445c0c60f052861355715feda39ca7ba969e2a
faiq_ahmad <ahmadfaiq46@gmail.com>
2025-07-28 20:35:46 +0500

    Basics

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/")
+def hello():
+    return {'message': 'Hello World'}
+
+@app.get("/about")
+def about():
+    return {'message': 'I am a Machine Learning Engineer'}
